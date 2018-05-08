53a54e484f068d70ee5db31ba3a0a8c2b9a59e2c
mrome007 <mrome007@ucr.edu>
2018-05-07 19:07:15 -0700

    Chapter 2

---
+++ Chapter2Variables.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+//var numberOfStopLights: Int = 4
+let numberOfStopLights: Int = 4
+//numberOfStopLights += 2
+var population: Int
+population = 5422
+var levelOfUnemployment: Int = 10
+
+let townName: String = "Knowhere"
+let townDescription = "\(townName) has a population of \(population), \(numberOfStopLights) stoplights, and \(levelOfUnemployment)% unemployment."
+
+// \() represents a placeholder in the string literal.
+print(townDescription)
+
