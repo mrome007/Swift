3ef1fcfd950cc002101041a624bdda91155d97ea
mrome007 <mrome007@ucr.edu>
2018-05-07 20:28:54 -0700

    Sprite Kit practice

--- Practice/Practice/GameScene.swift
+++ Practice/Practice/GameScene.swift
@@ -9,81 +9,30 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func didMove(to view: SKView) {
+class GameScene: SKScene
+{
+    override func didMove(to view: SKView)
+    {
+        //Like Vector3.zero in unity. Make the scene positoin from its lower left
+        //corner, regardless of any other settings:
+        self.anchorPoint = .zero
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        //Instantiate a constant(let), mySprite, instance of SKSpriteNode
+        //The SKSpriteNode constructor can set color and size
+        //Note: UIColor is a UIKit class with bilt in color presets
+        //Note: CGSize is a type we use to set node sizes.
+        let mySprite = SKSpriteNode(color: .blue, size: CGSize(width: 50, height: 50))
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        //Assign our sprite a position in points, relative to its
+        //parent node
+        mySprite.position = CGPoint(x: 150, y: 150)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        //Finally, we need to add our sprite node into the node tree.
+        //Call the SKScene's addChild function to add the node
+        //Note: In Swift, 'self' is an automatic property
+        //on any type instance, exactly equal to the instance itself
+        //So in this case, it refers to the GameScene instance.
+        //similar to "this" in C# or C++???
+        self.addChild(mySprite)
     }
 }
