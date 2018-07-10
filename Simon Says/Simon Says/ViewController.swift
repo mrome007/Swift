ac3e1111a099404bcb00f8780eb878f871301818
mrome007 <mrome007@ucr.edu>
2018-07-10 06:22:43 -0700

    Set up layout for Simon Says game.

---
+++ Simon Says/Simon Says/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simon Says
+//
+//  Created by Michael Romero on 7/10/18.
+//  Copyright © 2018 Michael Romero. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
