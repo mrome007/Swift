35bbf90c8568f4dbc00c0b9ea929519986617736
mrome007 <mrome007@ucr.edu>
2018-07-31 06:56:24 -0700

    Finish lockable text field.

---
+++ DelegatesChallenge/DelegatesChallenge/TextFieldDelegates/LockableTextFieldDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  LockableTextFieldDelegate.swift
+//  DelegatesChallenge
+//
+//  Created by Michael Romero on 7/31/18.
+//  Copyright © 2018 Michael Romero. All rights reserved.
+//
+
+import Foundation
--- DelegatesChallenge/DelegatesChallenge/ViewController.swift
+++ DelegatesChallenge/DelegatesChallenge/ViewController.swift
@@ -8,18 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var zipCodeTextField: UITextField!
+    @IBOutlet weak var cashTextField: UITextField!
+    @IBOutlet weak var lockableTextField: UITextField!
+    @IBOutlet weak var lockableSwitch: UISwitch!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.lockableTextField.delegate = self
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        textField.textColor = UIColor.black
+        
+        // Figure out what the new text will be, if we return true
+        var newText = textField.text! as NSString
+        newText = newText.replacingCharacters(in: range, with: string) as NSString
+        
+        // returning true gives the text field permission to change its text
+        return true;
     }
 
-
+    @IBAction func lockableSwitchChanged(_ sender: Any) {
+        lockableTextField.isUserInteractionEnabled = lockableSwitch.isOn
+        lockableTextField.textColor = lockableTextField.isUserInteractionEnabled ? UIColor.black : UIColor.lightGray
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.textColor = UIColor.black
+    }
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        return textField.isUserInteractionEnabled
+    }
 }
 
